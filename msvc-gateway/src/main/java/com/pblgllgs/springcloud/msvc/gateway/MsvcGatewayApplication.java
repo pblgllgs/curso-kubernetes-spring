a54965243cb1f32e9dd7ceef59c135394f06f1cf
pblgllgs <pbl.gllgs@gmail.com>
2023-02-02 17:24:24 -0300

    add msvc-auth

--- msvc-gateway/src/main/java/com/pblgllgs/springcloud/msvc/gateway/MsvcGatewayApplication.java
+++ msvc-gateway/src/main/java/com/pblgllgs/springcloud/msvc/gateway/MsvcGatewayApplication.java
@@ -2,7 +2,9 @@ package com.pblgllgs.springcloud.msvc.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsvcGatewayApplication {
 
