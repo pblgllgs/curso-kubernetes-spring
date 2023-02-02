a8a18c188a7bbce403b691dccb58f4312b438769
pblgllgs <pbl.gllgs@gmail.com>
2023-02-02 16:36:06 -0300

    add msvc-gateway and configure module in pow.xml parent

---
+++ msvc-gateway/src/test/java/com/pblgllgs/springcloud/msvc/gateway/MsvcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pblgllgs.springcloud.msvc.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
